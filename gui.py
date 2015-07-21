b2a9b568eb91e3c3d15fb06c90c218727d967ebc
Chen Chen <mail@roychan.org>
2015-07-21 18:39:52 +0800

    milestone

--- gui.py
+++ gui.py
@@ -1,25 +1,121 @@
 import simpy
-import random
+import random as rand
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import tkinter as GUI
+import threading
 
-##
-# Parameters needed
-# 
-# Choose:
-# - Probability distribution type of the bandwidth
-# - Probability distribution type of the packet generation frequency
-# - Accounting method
-# - Request method
-#
-# Input:
-# - Six parameters of two distributions
-# - Simulation time
-# - Buffer capacity (integer)
-# - Resource pool capacity (integer)
-##
+'''Core'''
+
+def generate_interval(num, parameter_one, parameter_two = 0.0, parameter_three = 0.0):
+    rand_intervals = [
+        rand.uniform,
+        rand.triangular,
+        rand.betavariate,
+        rand.expovariate,
+        rand.gammavariate,
+        rand.gauss,
+        rand.lognormvariate,
+        rand.normalvariate,
+        rand.vonmisesvariate,
+        rand.paretovariate,
+        rand.weibullvariate,
+    ]
+    try:
+        result = rand_intervals[num](parameter_one, parameter_two, parameter_three)
+    except TypeError:
+        try:
+            result = rand_intervals[num](parameter_one, parameter_two)
+        except TypeError:
+            result = rand_intervals[num](parameter_one)
+    return result
+
+def generate_priority(accounting_method, pk):
+    if accounting_method == 0:
+        return pk
+    elif accounting_method == 1:
+        return -pk
+    elif accounting_method == 2:
+        return rand.randint(1, 65535)
+
+def total_queue_load(link):
+    total_load = 0
+    for i in link:
+        total_load += len(i.queue)
+    return total_load
+
+def port_to_request(method, link, pk):
+    global BUFFER_CAPACITY
+    global RESOURCE_POOL_CAPACITY
+    port = None
+    if method == 0:
+        for i in link:
+            if port == None:
+                port = i
+            else:
+                if len(i.queue) + i.count < len(port.queue) + port.count:
+                    port = i
+        return port
+    elif method == 1:
+        port = link[rand.randint(0, len(link) - 1)]
+    elif method == 2:
+        for i in link:
+            if len(i.queue) < BUFFER_CAPACITY:
+                port = i
+                break
+            else:
+                continue
+    elif method == 3:
+        port = link[(pk - 1) % RESOURCE_POOL_CAPACITY]
+    return port
+
+def packet(env, pk, link):
+    global PACKET_GENERATION_DISTRIBUTION
+    global P_PARAMETER_ONE
+    global P_PARAMETER_TWO
+    global P_PARAMETER_THREE
+    global BANDWIDTH_DISTRIBUTION
+    global B_PARAMETER_ONE
+    global B_PARAMETER_TWO
+    global B_PARAMETER_THREE
+    global BUFFER_CAPACITY
+    global ACCOUNTING_METHOD
+    global REQUEST_METHOD
+    global packet_generated
+    global packet_lost
+    global total_queuing_delay
+    global total_transmission_delay
+    size = 1 / generate_interval(BANDWIDTH_DISTRIBUTION, B_PARAMETER_ONE, B_PARAMETER_TWO, B_PARAMETER_THREE)
+    print('Packet %d generated at %s, size = %s' % (pk, env.now, size))
+    packet_generated += 1
+    generation_time = float(env.now)
+    if total_queue_load(link) < BUFFER_CAPACITY * len(link):
+        port = port_to_request(REQUEST_METHOD, link, pk)
+        if len(port.queue) < BUFFER_CAPACITY:
+            with port.request(priority = generate_priority(ACCOUNTING_METHOD, pk)) as request:
+                yield request
+                transmission_start_time = env.now
+                total_queuing_delay += transmission_start_time - generation_time
+                print('Packet %d begin to transmit at %s on port %d' % (pk, env.now, link.index(port)))
+                yield env.timeout(size)
+                total_transmission_delay += size
+                print('Packet %d ended transmission at %s, cost %s' %(pk, env.now, size))
+        else:
+            packet_lost += 1
+            print('Packet %d lost at %s' % (pk, env.now))
+    else:
+        packet_lost += 1
+        print('Packet %d lost at %s' % (pk, env.now))
+
+def transmit(env, link):
+    pk = 1
+    while True:
+        env.process(packet(env, pk, link))
+        yield env.timeout(generate_interval(PACKET_GENERATION_DISTRIBUTION, P_PARAMETER_ONE, P_PARAMETER_TWO, P_PARAMETER_THREE))
+        pk += 1
+
+'''GUI'''
 
 ##
 # Main Frame
@@ -208,12 +304,101 @@ for i in range(len(request_methods)):
 request_method_frame.grid(column = 1, row = 3)
 
 ##
-# Control Buttons
+# Matplotlib
 ##
+
+graph_dict = {
+    'average_queuing_delay': [],
+    'average_transmission_delay': [],
+    'packet_lost_rate': [],
+    'throughput': []
+}
+
+for i in range(10000):
+    for key in graph_dict:
+        graph_dict[key].append(0)
+
+figure = plt.Figure(figsize=(5,4), dpi=100)
+'''aqd = figure.add_subplot(111)
+x = range(10)
+y = range(10)
+a.plot(x, y)'''
+
+aqd = figure.add_subplot(2, 2, 1)
+aqd.plot(range(10000), graph_dict['average_queuing_delay'])
+#aqd.ylabel('Average Queuing Delay')
+#aqd.xlabel('Time')
+plt.subplot(2, 2, 2)
+plt.plot(range(10000), graph_dict['average_transmission_delay'])
+plt.ylabel('Average Transmission Delay')
+plt.xlabel('Time')
+plt.subplot(2, 2, 3)
+plt.plot(range(10000), graph_dict['packet_lost_rate'])
+plt.ylabel('Packet Lost Rate')
+plt.xlabel('Time')
+plt.subplot(2, 2, 4)
+plt.plot(range(10000), graph_dict['throughput'])
+plt.ylabel('Throughput')
+plt.xlabel('Time')
+
+canvas = mpl.backends.backend_tkagg.FigureCanvasTkAgg(figure, master= main)
+#canvas.show()
+#canvas.get_tk_widget().grid(row = 1, column = 2, padx = 20)
+
+##
+# Control
+##
+
+def graph(env, graph_dict):
+    global packet_generated
+    global packet_lost
+    global total_queuing_delay
+    global total_transmission_delay
+    for i in range(10000):
+        try:
+            graph_dict['average_queuing_delay'][i] = (total_queuing_delay / (packet_generated - packet_lost))
+        except ZeroDivisionError:
+            graph_dict['average_queuing_delay'][i] = 0
+        try:
+            graph_dict['average_transmission_delay'][i] = (total_transmission_delay / (packet_generated - packet_lost))
+        except ZeroDivisionError:
+            graph_dict['average_transmission_delay'][i] = 0
+        try:
+            graph_dict['packet_lost_rate'][i] = (packet_lost / packet_generated * 100)
+        except ZeroDivisionError:
+            graph_dict['packet_lost_rate'][i] = 0
+        try:
+            graph_dict['throughput'][i] = (packet_generated - packet_lost) / env.now
+        except ZeroDivisionError:
+            graph_dict['throughput'][i] = 0
+        yield env.timeout(1)
+
+def start_simulation():
+    global graph_dict
+    global canvas
+    env = simpy.Environment()
+    link = []
+    for i in range(RESOURCE_POOL_CAPACITY):
+        link.append(simpy.PriorityResource(env, capacity = 1))
+    env.process(transmit(env, link))
+    env.process(graph(env, graph_dict))
+    env.run(until = 10000)
+    canvas.show()
+    canvas.get_tk_widget().grid(row = 1, column = 2, padx = 20)
+    print('Total packet generated', packet_generated)
+    print('Total packet lost', packet_lost)
+    print('Total Queuing Delay', total_queuing_delay)
+    print('Total Transmission Delay', total_transmission_delay)
+    print('Average Queuing Dealy', str(total_queuing_delay / (packet_generated - packet_lost)))
+    print('Average Transmission Dealy', str(total_transmission_delay / (packet_generated - packet_lost)))
+    print('Packet lost rate', str(packet_lost / packet_generated * 100), '%')
+    print('Average Throughput', str((packet_generated - packet_lost) / 10000))
 
 def apply_changes():
     global temp_packet_generation_distribution
     global temp_bandwidth_distribution
+    global temp_accounting_method
+    global temp_request_method
     global PACKET_GENERATION_DISTRIBUTION
     global P_PARAMETER_ONE
     global P_PARAMETER_TWO
@@ -227,33 +412,85 @@ def apply_changes():
     global ACCOUNTING_METHOD
     global REQUEST_METHOD
     global RESOURCE_POOL_CAPACITY
+    global buffer_capacity_input
+    global resource_pool_capacity_input
+    global p_parameter_one_input
+    global p_parameter_two_input
+    global p_parameter_three_input
+    global b_parameter_one_input
+    global b_parameter_two_input
+    global b_parameter_three_input
 
-    temp = [temp_packet_generation_distribution, temp_bandwidth_distribution]
-    no_check = [PACKET_GENERATION_DISTRIBUTION, BANDWIDTH_DISTRIBUTION]
-    for i in range(len(no_check)):
-        no_check[i] = temp[i].get()
+    def is_integer(value):
+        try:
+            int(value)
+            return True
+        except:
+            return False
 
-    check_integer = [BUFFER_CAPACITY, ACCOUNTING_METHOD, REQUEST_METHOD]
-    check_float = [P_PARAMETER_ONE, P_PARAMETER_TWO, P_PARAMETER_THREE, B_PARAMETER_ONE, B_PARAMETER_TWO, B_PARAMETER_THREE]
+    def is_float(value):
+        try:
+            float(value)
+            return True
+        except:
+            return False
 
-GUI.Button(button_frame, text = 'Apply Changes').grid(column = 0, row = 0)
-GUI.Button(button_frame, text = 'Start').grid(column = 1, row = 0)
+    integer_input = [buffer_capacity_input, resource_pool_capacity_input]
+
+    for i in range(len(integer_input)):
+        if not is_integer(integer_input[i].get()):
+            print('Something wrong, please check your input. 1')
+            return
+
+    float_input = [
+        p_parameter_one_input, p_parameter_two_input, p_parameter_three_input,
+        b_parameter_one_input, b_parameter_two_input, b_parameter_three_input,
+    ]
+
+    for i in range(len(float_input)):
+        if not is_float(float_input[i].get()):
+            print('Something wrong, please check your input. 2')
+            return
+
+    temp = [temp_packet_generation_distribution, temp_bandwidth_distribution, temp_accounting_method, temp_request_method]
+
+    PACKET_GENERATION_DISTRIBUTION = int(temp[0].get())
+    BANDWIDTH_DISTRIBUTION = int(temp[1].get())
+    ACCOUNTING_METHOD = int(temp[2].get())
+    REQUEST_METHOD = int(temp[3].get())
+
+    P_PARAMETER_ONE = float(float_input[0].get())
+    P_PARAMETER_TWO = float(float_input[1].get())
+    P_PARAMETER_THREE = float(float_input[2].get())
+    B_PARAMETER_ONE = float(float_input[3].get())
+    B_PARAMETER_TWO = float(float_input[4].get())
+    B_PARAMETER_THREE = float(float_input[5].get())
+
+    BUFFER_CAPACITY = int(integer_input[0].get())
+    RESOURCE_POOL_CAPACITY = int(integer_input[1].get())
+
+    global_variables = {
+        'PACKET_GENERATION_DISTRIBUTION': PACKET_GENERATION_DISTRIBUTION,
+        'P_PARAMETER_ONE': P_PARAMETER_ONE,
+        'P_PARAMETER_TWO': P_PARAMETER_TWO,
+        'P_PARAMETER_THREE': P_PARAMETER_THREE,
+        'BANDWIDTH_DISTRIBUTION': BANDWIDTH_DISTRIBUTION,
+        'B_PARAMETER_ONE': B_PARAMETER_ONE,
+        'B_PARAMETER_TWO': B_PARAMETER_TWO,
+        'B_PARAMETER_THREE': B_PARAMETER_THREE,
+        'BUFFER_CAPACITY': BUFFER_CAPACITY,
+        'RESOURCE_POOL_CAPACITY': RESOURCE_POOL_CAPACITY,
+        'ACCOUNTING_METHOD': ACCOUNTING_METHOD,
+        'REQUEST_METHOD': REQUEST_METHOD,
+    }
+
+    for key in global_variables:
+        print(key, '->', global_variables[key])
+
+GUI.Button(button_frame, text = 'Apply Changes', command = apply_changes).grid(column = 0, row = 0)
+GUI.Button(button_frame, text = 'Start', command = start_simulation).grid(column = 1, row = 0)
 GUI.Button(button_frame, text = 'Pause').grid(column = 2, row = 0)
 button_frame.grid(row = 4, columnspan = 2, pady = 10)
-
-##
-# Matplotlib
-##
-
-f = plt.Figure(figsize=(5,4), dpi=100)
-a = f.add_subplot(111)
-x = range(10)
-y = range(10)
-a.plot(x, y)
-
-canvas = mpl.backends.backend_tkagg.FigureCanvasTkAgg(f, master= main)
-canvas.show()
-canvas.get_tk_widget().grid(row = 1, column = 2, padx = 20)
 
 ##
 # Start Main loop
